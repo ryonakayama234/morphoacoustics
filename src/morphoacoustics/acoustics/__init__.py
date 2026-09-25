760416d49e220f35bff205c426e68719795c29d0
ryonakayama <zhongshanling468@gmail.com>
2026-09-25 19:00:36 +0900

    Expose Fidelity-0 impedance request and response

--- src/morphoacoustics/acoustics/__init__.py
+++ src/morphoacoustics/acoustics/__init__.py
@@ -1,13 +1,14 @@
 """Acoustic models, backend contracts, and solver primitives."""
 
-from .backend import SegmentedTubeBackend
-from .protocol import AcousticBackend, AcousticResponse
+from .backend import ImpedanceRequest, ImpedanceResponse, SegmentedTubeBackend
+from .protocol import AcousticBackend
 from .segmented_tube import SegmentedTube
 from .uniform_tube import UniformTube
 
 __all__ = [
     "AcousticBackend",
-    "AcousticResponse",
+    "ImpedanceRequest",
+    "ImpedanceResponse",
     "SegmentedTube",
     "SegmentedTubeBackend",
     "UniformTube",
