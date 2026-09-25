84668ba15832d74294f1339985158a92c69b3b32
ryonakayama <zhongshanling468@gmail.com>
2026-09-25 16:14:53 +0900

    Export segmented tube acoustics

--- src/morphoacoustics/acoustics/__init__.py
+++ src/morphoacoustics/acoustics/__init__.py
@@ -1,5 +1,6 @@
 """Acoustic models and solver primitives."""
 
+from .segmented_tube import SegmentedTube
 from .uniform_tube import UniformTube
 
-__all__ = ["UniformTube"]
+__all__ = ["SegmentedTube", "UniformTube"]
